a49a0cbc9a2ab9f30812e717f80718758c85b845
Leonardo Machado da Silva <631610260@senacinformatica.br>
2023-03-27 21:45:59 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# INF3N212-Carro
---
+++ src/inf3n212/carro/INF3N212Carro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inf3n212.carro;
+
+/**
+ *
+ * @author jbferraz
+ */
+public class INF3N212Carro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
