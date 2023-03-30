a5c3bd0916153996dccc913ea1584c88e2741f3d
Leonardo Machado da Silva <631610260@senacinformatica.br>
2023-03-29 21:48:50 -0300

    Atualização da classe principal e metodos de Menu Principal e SubMenu

--- src/inf3n212/carro/INF3N212Carro.java
+++ src/inf3n212/carro/INF3N212Carro.java
@@ -6,16 +6,19 @@ package inf3n212.carro;
 
 import controller.CCarro;
 import controller.CPessoa;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
  * @author jbferraz
  */
 public class INF3N212Carro {
-    
+
     public static CPessoa cadPessoa = new CPessoa();
     public static CCarro cadCarro = new CCarro();
-    
+    static Scanner leia = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
@@ -23,10 +26,49 @@ public class INF3N212Carro {
         // TODO code application logic here
         cadPessoa.mockPessoas();
         cadCarro.mockCarros();
-        
+
         System.out.println(cadPessoa.getPessoas());
         System.out.println("---");
         System.out.println(cadCarro.getCarros());
+
+    }//fim método main 
+
+    public static int leiaNumInt() {
+        Scanner leiaNum = new Scanner(System.in);
+        try {
+            return leiaNum.nextInt();
+        } catch (InputMismatchException i) {
+            System.out.println("Erro: "+i.getMessage() + "\nTente Novamente!");
+            leiaNumInt();
+        }
+        return 99;
+        }
+    
+    public static void menuP(){
+        System.out.println("-- Menu Principal --");
+        System.out.println("1 - Ger. Pessoa");
+        System.out.println("2 - Ger. Carro");
+        System.out.println("0 - Sair");
+        System.out.print("Digite aqui: ");
+    }//fim menuP
+    
+    public static void subMenu(int opM){
+        String subM = null;
+        if (opM==1) {
+            subM = "Pessoa";
+            
+        }
+        if (opM==2) {
+            subM = "Carro";
+        }
+        
+        System.out.println("-- Ger. "+ subM + "--");
+        System.out.println("1 - Cadastrar "+ subM);
+        System.out.println("2 - Editar " + subM);
+        System.out.println("3 - Listar " + subM + "s");
+        System.out.println("4 - Deletar " + subM);
+        System.out.println("0 - Voltar");
+        System.out.print("Digite aqui: ");
     }
     
-}
+    }//fim classe
